268a263611b7f65c84cf1bb7de8cf6fc9f8d3322
TheAmericanEmu <harringtondingc@gmail.com>
2026-01-15 12:38:31 -0600

    ffff

--- src/camera_3d.gd
+++ src/camera_3d.gd
@@ -3,20 +3,22 @@ extends Camera3D
 
 @onready var grid_map: GridMap = $"../GridMap"
 
-func find_click_pos():
+func find_click_pos()->Vector3:
 	var space_state = get_world_3d().direct_space_state
 	var cam = self
 	var mousepos = get_viewport().get_mouse_position()
 	var origin = cam.project_ray_origin(mousepos)
-	var end = origin + cam.project_ray_normal(mousepos) * 100000
+	var end = origin + cam.project_ray_normal(mousepos) * 100
 	var query = PhysicsRayQueryParameters3D.create(origin, end)
 	query.collision_mask=2
 	query.collide_with_bodies=true
 	query.collide_with_areas = false
 	var result = space_state.intersect_ray(query)
 	if result:
-		var pos = result["position"]
-		self.look_at(pos)
+		var pos:Vector3 = result["position"]
+		return pos
+	return Vector3.ZERO
+
 
 
 func _ready() -> void:
---
+++ src/turret_base.gd
@@ -0,0 +1,5 @@
+extends StaticBody3D
+
+
+func _process(delta: float) -> void:
+	print("hello")
